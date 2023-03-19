4faf0317361d9e870a27059626abbcd3cf785496
Lina <Lina>
2023-03-19 18:08:18 +0100

    MVP5: Cleaned the code

--- poke_api.py
+++ poke_api.py
@@ -14,62 +14,75 @@ logger.addHandler(console_handler)
 
 
 def save_response(data: list, file_name: str, directory_name: str) -> None:
+    logger.info("Saving IDS in a file.")
+
     for version in data:
         filename = os.path.join(f"{directory_name}", f"{file_name}")
-        with open(filename, "w") as file:
+        with open(file=filename, mode="w", encoding="utf-8") as file:
             try:
                 json.dump(data, file)
-            except Exception as error:
-                print(f"Failed to save file {filename}: {error}")
+            except IOError as error:
+                logger.error("Failed to save file %s : %s", filename, error)
 
     return None
 
 
 def split_data(data: list, directory_name: str):
-    increment = 20
+    increment = 20  # TODO: Based on what 20??
+
     chunks = [data[i : i + increment] for i in range(0, len(data), increment)]
 
     for i, chunk in enumerate(chunks):
-        with open(f"{directory_name}/pokemon_file_{i+1}.json", "w") as f:
-            json.dump(chunk, f)
+        with open(
+            file=f"{directory_name}/pokemon_file_{i+1}.json", mode="w", encoding="utf-8"
+        ) as file_name:
+            json.dump(chunk, file_name)
 
 
 def get_pokemon_species_payload(
     starting_offset: int, payload_limit: int, session: requests.Session
 ) -> dict:
-    all_url = "https://pokeapi.co/api/v2/pokemon-species/"
+    pokemon_species_url = "https://pokeapi.co/api/v2/pokemon-species/"
 
     try:
         pokemon_species_dict_response = session.get(
-            f"{all_url}?offset={starting_offset}&limit={payload_limit}"
+            f"{pokemon_species_url}?offset={starting_offset}&limit={payload_limit}"
         ).json()
     except requests.exceptions.ConnectionError:
         time.sleep(secs=2**starting_offset)
 
-        logger.info(f"Retrying to get pokemon_species payload")
+        logger.info("Retrying to get pokemon_species payload")
 
         pokemon_species_dict_response = get_pokemon_species_payload(
-            offset=starting_offset, limit=payload_limit, session=session
+            starting_offset=starting_offset,
+            payload_limit=payload_limit,
+            session=session,
         )
     return pokemon_species_dict_response
 
 
-def filter_pokemon_data(pokemon_data: dict, game_version: list, offset: int) -> dict:
+def filter_pokemon_data(
+    pokemon_data: dict, game_version: list, starting_offset: int
+) -> dict:
     pokemon_by_version = {version: [] for version in game_version}
 
     for pokemon in pokemon_data["results"]:
         individual_pokemon = pokemon["url"]
         try:
-            pokemon_info_dict_response = requests.get(url=individual_pokemon).json()
+            pokemon_info_dict_response = requests.get(
+                url=individual_pokemon
+            ).json()  # TODO: consider timeouts?
         except requests.exceptions.ConnectionError:
-            time.sleep(secs=2**offset)
+            time.sleep(secs=2**starting_offset)
             continue
+
         if len(pokemon_info_dict_response["flavor_text_entries"]) > 0:
             for flavor_text_entry in pokemon_info_dict_response["flavor_text_entries"]:
                 version_name = flavor_text_entry["version"]["name"]
                 if version_name in game_version:
                     url = pokemon["url"].split("/")[-2]
                     pokemon_by_version[version_name].append(url)
+
     return pokemon_by_version
 
 
@@ -90,11 +103,13 @@ def get_pokemon_in_versions(
             session=session,
         )
 
-        pokemon_by_version = filter_pokemon_data(
-            pokemon_data=pokemon_ids, game_version=game_version, offset=starting_offset
+        processed_pokemon_by_version = filter_pokemon_data(
+            pokemon_data=pokemon_ids,
+            game_version=game_version,
+            starting_offset=starting_offset,
         )
 
-        for version, pokemon_list in pokemon_by_version.items():
+        for version, pokemon_list in processed_pokemon_by_version.items():
             pokemon_urls.update(pokemon_list)
 
         if not pokemon_ids["next"]:
@@ -108,7 +123,8 @@ def get_pokemon_in_versions(
 def process_pokemon(session: requests.Session, offset: int, pokemon_ids: set) -> list:
     pokemon_url = "https://pokeapi.co/api/v2/pokemon"
     pokemon_list = []
-    pokemon_ids = pokemon_ids
+
+    logger.info("Extracting data for each pokemon based on their ID's...")
 
     for pokemon in pokemon_ids:
         while True:
@@ -119,7 +135,7 @@ def process_pokemon(session: requests.Session, offset: int, pokemon_ids: set) ->
                 time.sleep(2**offset)
                 continue
 
-        wanted_slots = [1, 2]  # list
+        wanted_slots = [1, 2]
 
         slots = {
             type_dict.get("slot"): type_dict.get("type").get("name")
@@ -141,8 +157,6 @@ def process_pokemon(session: requests.Session, offset: int, pokemon_ids: set) ->
 
         if len(slots) > 1:
             stats["slot 2"] = slots.get(2)
-
-            # Move "slot 2" after "slot 1" in the dictionary
             new_stats = {}
             for key, value in stats.items():
                 if key == "slot 1":
@@ -165,7 +179,7 @@ if __name__ == "__main__":
     SESSION = requests.Session()
     GAME_VERSIONS = ["red", "blue", "leafgreen", "white"]
 
-    logger.info(f"Getting pokemon from selected game versions...")
+    logger.info("Getting pokemon from selected game versions ...")
 
     pokemon_by_version = get_pokemon_in_versions(
         starting_offset=OFFSET,
@@ -174,22 +188,21 @@ if __name__ == "__main__":
         game_version=GAME_VERSIONS,
     )
 
+    logger.info("Successfully obtained pokemon ids for selected game versions. ")
+    logger.info("There are %s in selected pokemon games.", len(pokemon_by_version))
+
     save_response(
         data=list(pokemon_by_version),
         file_name="pokemon_ids.json",
         directory_name="filtered_pokemon_ids",
     )
 
-    print(len(pokemon_by_version))
-
+    logger.info("Starting to process pokemon data ...")
     processed_pokemon = process_pokemon(
         session=SESSION, offset=OFFSET, pokemon_ids=pokemon_by_version
     )
 
-    print(len(processed_pokemon))
+    logger.info("Successfully processed %s pokemon.", len(processed_pokemon))
 
+    logger.info("Saving data...")
     split_data(data=processed_pokemon, directory_name="pokemon_stats")
-
-    # split_json(data=processed_pokemon)
-
-    # take those ids and create files
